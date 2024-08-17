120182f7fddf445aa824175608d4e7b944db0e65
Familia <Familia@RodrAvilHP>
2024-08-17 10:13:29 -0500

    ADELANTO CONTROLADOR --- POR MARIO

--- Proyecto_Programacion_1/src/co/edu/unbosque/control/Controlador.java
+++ Proyecto_Programacion_1/src/co/edu/unbosque/control/Controlador.java
@@ -1,12 +1,139 @@
 package co.edu.unbosque.control;
 
+import co.edu.unbosque.model.CarneFria;
+import co.edu.unbosque.model.ModelFacade;
+import co.edu.unbosque.view.ViewFacade;
+
 public class Controlador {
+
+	private ModelFacade mf;
+	private ViewFacade vf;
 
 	public Controlador() {
 		run();
 	}
 
 	public void run() {
+
+		mainloop: while (true) {
+
+			mostrarMenuPrincipal();
+			int op = vf.getCon().readInt();
+
+			switch (op) {
+
+			case 1:
+				mostrarMenuCarne();
+				break;
+
+			case 2:
+				mostrarMenuFrutaVerdura();
+				break;
+
+			case 3:
+				mostrarMenuJuguete();
+				break;
+
+			case 4:
+				mostrarMenuPanaderia();
+				break;
+
+			case 5:
+				vf.getCon().printNewLine("Te esperamos pronto!");
+				break mainloop;
+
+			default:
+				vf.getCon().printNewLine("INGRESE UNA OPCION VALIDA");
+
+				break;
+			}
+
+		}
+
+	}
+
+	public void mostrarMenuPrincipal() {
+
+		String menuPpal = """
+
+				BIENVENDO AL MENU PRINCIPAL DE ALMACENES EXITO!
+
+				*Ingrese el solo el número de la opción deseada*
+
+				1) Administrar carnes frías
+				2) Administrar frutas y verduras
+				3) Administrar juguetes
+				4) Administrar la panadería
+
+				5) SALIR
+
+				""";
+
+		vf.getCon().printNewLine(menuPpal);
+
+	}
+
+	public void mostrarMenuCarne() {
+
+		meatloop: while (true) {
+
+			vf.getCon().printNewLine("---MENU CARNES FRIAS---");
+			vf.getCon().printNewLine("\n1) Agregar\n2) Mostrar\n3) Actualizar\n4) Eliminar \5) Volver");
+			int op1 = vf.getCon().readInt();
+
+			switch (op1) {
+			case 1:
+				vf.getCon().printNewLine("---AGREGANDO CARNE FRIA---");
+
+				vf.getCon().printSameLine("Ingrese el número de identificación del producto: ");
+				long numid = vf.getCon().readLong();
+
+				vf.getCon().printSameLine("Ingrese el nombre del producto: ");
+				String nombre = vf.getCon().readLine();
+
+				vf.getCon().printSameLine("Ingrese el nombre de la empresa del producto: ");
+				String empresa = vf.getCon().readLine();
+
+				vf.getCon().printSameLine("Ingrese el precio del producto: ");
+				float precio = vf.getCon().readFloat();
+
+				vf.getCon().printSameLine("Ingrese la cantidad del producto: ");
+				int cantidad = vf.getCon().readInt();
+
+				vf.getCon().printSameLine("Ingrese el nombre del animal de origen del producto: ");
+				String animalOrigen = vf.getCon().readLine();
+
+				mf.getCarneFriaDAO().crear(new CarneFria(op1, nombre, empresa, precio, cantidad, animalOrigen));
+				break;
+
+			case 2:
+				vf.getCon().printNewLine("---MOSTRANDO LISTA CARNES FRIAS---");
+				mf.getCarneFriaDAO().mostrar();
+
+				break;
+
+			case 5:
+				vf.getCon().printNewLine("---VOLVIENDO A MENU PRINCIPAL---");
+				break meatloop;
+
+			default:
+				vf.getCon().printNewLine("INGRESE UNA OPCION VALIDA");
+
+				break;
+			}
+
+		}
+	}
+
+	public void mostrarMenuFrutaVerdura() {
+
+	}
+
+	public void mostrarMenuJuguete() {
+
+	}
+
+	public void mostrarMenuPanaderia() {
 
 	}
 
