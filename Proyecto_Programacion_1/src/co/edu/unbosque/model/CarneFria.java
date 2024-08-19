328e4780238aaa5c6669b11e6945b8481607c152
emman <emman@Santiagopinzon1>
2024-08-19 07:24:58 -0500

    comentarios javadoc CarneFria emmanuel

--- Proyecto_Programacion_1/src/co/edu/unbosque/model/CarneFria.java
+++ Proyecto_Programacion_1/src/co/edu/unbosque/model/CarneFria.java
@@ -1,38 +1,83 @@
 package co.edu.unbosque.model;
 
+/**
+ * La clase CarneFria representa un producto del tipo carne fría, que hereda de la clase Producto.
+ * Además de los atributos heredados, incluye el atributo animalOrigen, que indica el animal de donde proviene la carne.
+ */
 public class CarneFria extends Producto {
 
-	private String animalOrigen;
+    private String animalOrigen;
 
-	public CarneFria() {
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * Constructor por defecto de la clase CarneFria.
+     */
+    public CarneFria() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public CarneFria(String animalOrigen) {
-		super();
-		this.animalOrigen = animalOrigen;
-	}
+    /**
+     * Constructor que inicializa la clase CarneFria con el animal de origen.
+     *
+     * @param animalOrigen El nombre del animal de donde proviene la carne.
+     */
+    public CarneFria(String animalOrigen) {
+        super();
+        this.animalOrigen = animalOrigen;
+    }
 
-	public CarneFria(long numId, String nombre, String empresa, float precio, int cantidad, String animalOrigen) {
-		super(numId, nombre, empresa, precio, cantidad);
-		this.animalOrigen = animalOrigen;
-	}
+    /**
+     * Constructor que inicializa todos los atributos de la clase CarneFria, incluyendo los heredados.
+     *
+     * @param numId       El número de identificación del producto.
+     * @param nombre      El nombre del producto.
+     * @param empresa     El nombre de la empresa que produce el producto.
+     * @param precio      El precio del producto.
+     * @param cantidad    La cantidad disponible del producto.
+     * @param animalOrigen El nombre del animal de donde proviene la carne.
+     */
+    public CarneFria(long numId, String nombre, String empresa, float precio, int cantidad, String animalOrigen) {
+        super(numId, nombre, empresa, precio, cantidad);
+        this.animalOrigen = animalOrigen;
+    }
 
-	public CarneFria(long numId, String nombre, String empresa, float precio, int cantidad) {
-		super(numId, nombre, empresa, precio, cantidad);
-	}
+    /**
+     * Constructor que inicializa todos los atributos heredados de la clase Producto, sin especificar el animal de origen.
+     *
+     * @param numId    El número de identificación del producto.
+     * @param nombre   El nombre del producto.
+     * @param empresa  El nombre de la empresa que produce el producto.
+     * @param precio   El precio del producto.
+     * @param cantidad La cantidad disponible del producto.
+     */
+    public CarneFria(long numId, String nombre, String empresa, float precio, int cantidad) {
+        super(numId, nombre, empresa, precio, cantidad);
+    }
 
-	public String getAnimalOrigen() {
-		return animalOrigen;
-	}
+    /**
+     * Obtiene el animal de origen de la carne.
+     *
+     * @return El nombre del animal de origen.
+     */
+    public String getAnimalOrigen() {
+        return animalOrigen;
+    }
 
-	public void setAnimalOrigen(String animalOrigen) {
-		this.animalOrigen = animalOrigen;
-	}
+    /**
+     * Establece el animal de origen de la carne.
+     *
+     * @param animalOrigen El nombre del animal de origen a establecer.
+     */
+    public void setAnimalOrigen(String animalOrigen) {
+        this.animalOrigen = animalOrigen;
+    }
 
-	@Override
-	public String toString() {
-		return super.toString() + "\nAnimal de origen: " + animalOrigen;
-	}
-
+    /**
+     * Devuelve una representación en cadena de la información de la carne fría, incluyendo los atributos heredados y el animal de origen.
+     *
+     * @return Una cadena con la información del producto.
+     */
+    @Override
+    public String toString() {
+        return super.toString() + "\nAnimal de origen: " + animalOrigen;
+    }
 }
