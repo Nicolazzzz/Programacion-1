1e342d5cd3d14bba2be6031e208bb84a1a5b075f
emman <emman@Santiagopinzon1>
2024-08-19 22:11:46 -0500

    avance javadoc controlador linea 432 emmanuel

--- Proyecto_Programacion_1/src/co/edu/unbosque/control/Controlador.java
+++ Proyecto_Programacion_1/src/co/edu/unbosque/control/Controlador.java
@@ -1,3 +1,6 @@
+/**
+ * Paquete que contiene las clases relacionadas con el control de la aplicación y en este paquete se maneja la logica de el aplicativo.
+ */
 package co.edu.unbosque.control;
 
 import java.awt.Image;
@@ -19,6 +22,11 @@ import co.edu.unbosque.model.ModelFacade;
 import co.edu.unbosque.model.Panaderia;
 import co.edu.unbosque.view.ViewFacade;
 
+/**
+ * Clase que controla la interacción y logica entre la vista y el modelo en la
+ * aplicación. Implementa la interfaz {@link java.awt.event.ActionListener} para
+ * manejar eventos de acciones en la interfaz gráfica.
+ */
 public class Controlador implements ActionListener {
 
 	private ModelFacade mf;
@@ -34,6 +42,10 @@ public class Controlador implements ActionListener {
 	private boolean newjuguete = false;
 	private boolean newpan = false;
 
+	/**
+	 * Constructor que inicializa el controlador, Modelfacade, ViewFacade y muestra
+	 * el menú principal.
+	 */
 	public Controlador() {
 		mf = new ModelFacade();
 		vf = new ViewFacade();
@@ -42,9 +54,13 @@ public class Controlador implements ActionListener {
 		asignarLectores();
 	}
 
+	/**
+	 * Asigna los ActionListeners a los botones de los paneles de la vista. Esto
+	 * permite que el controlador maneje los eventos de acción generados por la
+	 * interfaz gráfica.
+	 */
 	public void asignarLectores() {
-
-		// MENU PRINCIPAL
+		// Asigna listeners a los botones del menú principal
 		vf.getPrincipal().getMenuPpal().getBtnCarne().addActionListener(this);
 		vf.getPrincipal().getMenuPpal().getBtnCarne().setActionCommand("CARNEFRIA");
 
@@ -57,7 +73,7 @@ public class Controlador implements ActionListener {
 		vf.getPrincipal().getMenuPpal().getBtnPan().addActionListener(this);
 		vf.getPrincipal().getMenuPpal().getBtnPan().setActionCommand("PAN");
 
-		// MENU GESTION
+		// Asigna listeners a los botones del menú de gestión
 		vf.getPrincipal().getMenuGestion().getBtnAgregar().addActionListener(this);
 		vf.getPrincipal().getMenuGestion().getBtnAgregar().setActionCommand("AGREGAR");
 
@@ -73,7 +89,7 @@ public class Controlador implements ActionListener {
 		vf.getPrincipal().getMenuGestion().getBtnVolver().addActionListener(this);
 		vf.getPrincipal().getMenuGestion().getBtnVolver().setActionCommand("VOLVER");
 
-		// PANEL AGREGAR
+		// Asigna listeners a los botones del panel de agregar
 		vf.getPrincipal().getPanelAgregar().getBtnAgregar().addActionListener(this);
 		vf.getPrincipal().getPanelAgregar().getBtnAgregar().setActionCommand("AGREGARPRODUCTO");
 
@@ -85,220 +101,340 @@ public class Controlador implements ActionListener {
 
 		vf.getPrincipal().getPanelAgregar().getBtnActualizar().addActionListener(this);
 		vf.getPrincipal().getPanelAgregar().getBtnActualizar().setActionCommand("UPDATE");
-
 	}
 
+	/**
+	 * Método que maneja los eventos de acción generados por los componentes de la
+	 * interfaz gráfica. Este método se encarga de actualizar la interfaz de usuario
+	 * dependiendo del componente seleccionado en el menú principal o en otros
+	 * paneles, y ajusta la configuración y visibilidad de varios componentes
+	 * gráficos según la sección que el usuario desea administrar.
+	 *
+	 * @param e El evento de acción que se produjo.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
 		switch (e.getActionCommand()) {
 
-		// MENU PRINCIPAL
+		// Maneja la selección de "Carne Fría" desde el menú principal
 		case "CARNEFRIA":
+			// Cambia el título de la ventana para indicar que se está administrando "Carnes
+			// Frías"
 			vf.getPrincipal().setTitle("ADMINISTRANDO CARNES FRIAS");
-			// labels fondo
+
+			// Configura la visibilidad de las imágenes de fondo según el tipo de producto
+			// seleccionado
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarC().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarV().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarJ().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarP().setVisible(false);
 
+			// Ajusta las imágenes específicas del menú de gestión para mostrar la sección
+			// "Carne Fría"
 			vf.getPrincipal().getMenuGestion().getImagenCarne().setVisible(true);
 			vf.getPrincipal().getMenuGestion().getImagenFV().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenJuguete().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenPan().setVisible(false);
 
+			// Configura los campos de atributos propios visibles y no visibles para "Carne
+			// Fría"
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio3().setVisible(false);
 
+			// Muestra el panel del menú de gestión donde se realizarán las operaciones CRUD
+			// para "Carne Fría"
 			vf.getPrincipal().mostrarPanelMenuGestion();
+
+			// Ajusta las variables booleanas para indicar que se está gestionando "Carne
+			// Fría"
 			carne = true;
 			frutaV = false;
 			juguete = false;
 			pan = false;
 			break;
 
+		// Maneja la selección de "Fruta y Verdura" desde el menú principal
 		case "FRUTAVERDURA":
+			// Cambia el título de la ventana para indicar que se está administrando "Frutas
+			// y Verduras"
 			vf.getPrincipal().setTitle("ADMINISTRANDO FRUTAS Y VERDURAS");
 
+			// Configura la visibilidad de las imágenes de fondo según el tipo de producto
+			// seleccionado
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarC().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarV().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarJ().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarP().setVisible(false);
 
+			// Ajusta las imágenes específicas del menú de gestión para mostrar la sección
+			// "Fruta y Verdura"
 			vf.getPrincipal().getMenuGestion().getImagenCarne().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenFV().setVisible(true);
 			vf.getPrincipal().getMenuGestion().getImagenJuguete().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenPan().setVisible(false);
 
+			// Configura los campos de atributos propios visibles para "Fruta y Verdura"
+			// En este caso, muestra dos campos para atributos específicos como fecha de
+			// cosecha y orgánico
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio3().setVisible(false);
 
+			// Define los textos predeterminados para los campos de atributos propios
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setText("dd/mm/yyyy");
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setText("Si / No");
 
+			// Muestra el panel del menú de gestión donde se realizarán las operaciones CRUD
+			// para "Fruta y Verdura"
 			vf.getPrincipal().mostrarPanelMenuGestion();
+
+			// Ajusta las variables booleanas para indicar que se está gestionando "Fruta y
+			// Verdura"
 			carne = false;
 			frutaV = true;
 			juguete = false;
 			pan = false;
 			break;
 
+		// Maneja la selección de "Juguete" desde el menú principal
 		case "JUGUETE":
+			// Cambia el título de la ventana para indicar que se está administrando
+			// "Juguetes"
 			vf.getPrincipal().setTitle("ADMINISTRANDO JUGUETES");
-			// labels fondo
+
+			// Configura la visibilidad de las imágenes de fondo según el tipo de producto
+			// seleccionado
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarC().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarV().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarJ().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarP().setVisible(false);
 
+			// Ajusta las imágenes específicas del menú de gestión para mostrar la sección
+			// "Juguete"
 			vf.getPrincipal().getMenuGestion().getImagenCarne().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenFV().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenJuguete().setVisible(true);
 			vf.getPrincipal().getMenuGestion().getImagenPan().setVisible(false);
 
+			// Configura los campos de atributos propios visibles para "Juguete"
+			// Muestra dos campos para atributos específicos de juguetes, como edad
+			// recomendada y tipo de juguete
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio3().setVisible(false);
 
+			// Muestra el panel del menú de gestión donde se realizarán las operaciones CRUD
+			// para "Juguete"
 			vf.getPrincipal().mostrarPanelMenuGestion();
+
+			// Ajusta las variables booleanas para indicar que se está gestionando "Juguete"
 			carne = false;
 			frutaV = false;
 			juguete = true;
 			pan = false;
 			break;
 
+		// Maneja la selección de "Panadería" desde el menú principal
 		case "PAN":
+			// Cambia el título de la ventana para indicar que se está administrando
+			// "Panadería"
 			vf.getPrincipal().setTitle("ADMINISTRANDO PANADERIA");
-			// labels fondo
+
+			// Configura la visibilidad de las imágenes de fondo según el tipo de producto
+			// seleccionado
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarC().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarV().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarJ().setVisible(false);
 			vf.getPrincipal().getPanelAgregar().getImagenAgregarP().setVisible(true);
 
+			// Ajusta las imágenes específicas del menú de gestión para mostrar la sección
+			// "Panadería"
 			vf.getPrincipal().getMenuGestion().getImagenCarne().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenFV().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenJuguete().setVisible(false);
 			vf.getPrincipal().getMenuGestion().getImagenPan().setVisible(true);
 
+			// Configura los campos de atributos propios visibles para "Panadería"
+			// Muestra tres campos para atributos específicos de panadería, como sin gluten,
+			// integral y sin azúcar
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setVisible(true);
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio3().setVisible(true);
 
+			// Define los textos predeterminados para los campos de atributos propios
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setText("Si / No");
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setText("Si / No");
 			vf.getPrincipal().getPanelAgregar().getAtributoPropio3().setText("Si / No");
 
+			// Muestra el panel del menú de gestión donde se realizarán las operaciones CRUD
+			// para "Panadería"
 			vf.getPrincipal().mostrarPanelMenuGestion();
+
+			// Ajusta las variables booleanas para indicar que se está gestionando
+			// "Panadería"
 			carne = false;
 			frutaV = false;
 			juguete = false;
 			pan = true;
 			break;
 
-		// MENU GESTION
-		case "AGREGAR":
+		/**
+		 * Método que maneja los eventos de acción relacionados con la gestión de
+		 * productos en la interfaz gráfica, tales como agregar, mostrar y actualizar
+		 * elementos en la base de datos.
+		 *
+		 * @param e El evento de acción que se produjo.
+		 */
 
+		// Maneja la acción de "Agregar" un nuevo producto a la base de datos
+		case "AGREGAR":
+			// Oculta el botón de "Actualizar" ya que estamos en modo de agregación
 			vf.getPrincipal().getPanelAgregar().getBtnActualizar().setVisible(false);
 
-			if (carne == true) {
+			// Determina qué tipo de producto se va a agregar basado en la sección
+			// seleccionada
+			if (carne) {
+				// Cambia el título de la ventana para indicar que se está agregando un nuevo
+				// producto de "Carnes Frías"
 				vf.getPrincipal().setTitle("AGREGANDO CARNES FRIAS");
+				// Muestra el panel de agregación para ingresar los datos del nuevo producto
 				vf.getPrincipal().mostrarPanelAgregar();
 			}
-			if (frutaV == true) {
+			if (frutaV) {
+				// Cambia el título de la ventana para indicar que se está agregando un nuevo
+				// producto de "Frutas y Verduras"
 				vf.getPrincipal().setTitle("AGREGANDO FRUTAS Y VERDURAS");
+				// Muestra el panel de agregación para ingresar los datos del nuevo producto
 				vf.getPrincipal().mostrarPanelAgregar();
-
 			}
-			if (juguete == true) {
+			if (juguete) {
+				// Cambia el título de la ventana para indicar que se está agregando un nuevo
+				// "Juguete"
 				vf.getPrincipal().setTitle("AGREGANDO JUGUETES");
+				// Muestra el panel de agregación para ingresar los datos del nuevo producto
 				vf.getPrincipal().mostrarPanelAgregar();
-
 			}
-			if (pan == true) {
+			if (pan) {
+				// Cambia el título de la ventana para indicar que se está agregando un nuevo
+				// producto en "Panadería"
 				vf.getPrincipal().setTitle("AGREGANDO EN PANADERIA");
+				// Muestra el panel de agregación para ingresar los datos del nuevo producto
 				vf.getPrincipal().mostrarPanelAgregar();
-
 			}
 			break;
 
+		// Maneja la acción de "Mostrar" la lista de productos almacenados
 		case "MOSTRAR":
-
-			if (carne == true) {
+			// Verifica la sección seleccionada y muestra el listado correspondiente
+			if (carne) {
+				// Muestra la lista de "Carnes Frías" en la interfaz
 				vf.getCon().mostrarListado(mf.getCarneFriaDAO().mostrar());
 			}
-			if (frutaV == true) {
+			if (frutaV) {
+				// Muestra la lista de "Frutas y Verduras" en la interfaz
 				vf.getCon().mostrarListado(mf.getFrutaVerduraDAO().mostrar());
-
 			}
-			if (juguete == true) {
+			if (juguete) {
+				// Muestra la lista de "Juguetes" en la interfaz
 				vf.getCon().mostrarListado(mf.getJugueteDAO().mostrar());
-
 			}
-			if (pan == true) {
+			if (pan) {
+				// Muestra la lista de productos en "Panadería" en la interfaz
 				vf.getCon().mostrarListado(mf.getPanaderiaDAO().mostrar());
 			}
 			break;
 
+		// Maneja la acción de "Actualizar" un producto existente en la base de datos
 		case "ACTUALIZAR":
 			try {
-
-				if (carne == true) {
+				// Verifica si la sección seleccionada es "Carnes Frías"
+				if (carne) {
+					// Cambia el título de la ventana para indicar que se está actualizando un
+					// producto en "Carnes Frías"
 					vf.getPrincipal().setTitle("ACTUALIZANDO CARNES FRIAS");
+					// Solicita al usuario que ingrese la posición del producto que desea actualizar
 					index = Integer.parseInt(vf.getCon().leerEntradaEmergente("Ingrese la posición a actualizar: "));
+					// Verifica si el índice ingresado es válido
 					if (mf.getCarneFriaDAO().checkearIndex(index - 1).equals("s")) {
+						// Oculta el botón de "Agregar" y muestra el botón de "Actualizar" para editar
+						// el producto
 						vf.getPrincipal().getPanelAgregar().getBtnAgregar().setVisible(false);
 						vf.getPrincipal().getPanelAgregar().getBtnActualizar().setVisible(true);
+						// Muestra el panel de actualización con los datos del producto seleccionado
 						vf.getPrincipal().mostrarPanelAgregar();
+						// Ajusta las variables booleanas para indicar que se está actualizando un
+						// producto de "Carnes Frías"
 						newcarne = true;
 						newfrutaV = false;
 						newjuguete = false;
 						newpan = false;
-
 					} else {
+						// Muestra un mensaje de alerta si el índice ingresado no es válido
 						vf.getCon().mostrarAlerta("Ingrese un indice valido, recuerde "
 								+ "que no puede ser negativo ni exceder el tamaño de la lista");
 					}
 				}
 
-				if (frutaV == true) {
+				// Verifica si la sección seleccionada es "Frutas y Verduras"
+				if (frutaV) {
+					// Configura los campos de atributos propios para "Frutas y Verduras"
 					vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setText("dd/mm/yyyy");
 					vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setText("Si / No");
+					// Cambia el título de la ventana para indicar que se está actualizando un
+					// producto en "Frutas y Verduras"
 					vf.getPrincipal().setTitle("ACTUALIZANDO FRUTAS Y VEGETALES");
+					// Solicita al usuario que ingrese la posición del producto que desea actualizar
 					index = Integer.parseInt(vf.getCon().leerEntradaEmergente("Ingrese la posición a actualizar: "));
+					// Verifica si el índice ingresado es válido
 					if (mf.getFrutaVerduraDAO().checkearIndex(index - 1).equals("s")) {
+						// Oculta el botón de "Agregar" y muestra el botón de "Actualizar" para editar
+						// el producto
 						vf.getPrincipal().getPanelAgregar().getBtnAgregar().setVisible(false);
 						vf.getPrincipal().getPanelAgregar().getBtnActualizar().setVisible(true);
+						// Muestra el panel de actualización con los datos del producto seleccionado
 						vf.getPrincipal().mostrarPanelAgregar();
+						// Ajusta las variables booleanas para indicar que se está actualizando un
+						// producto de "Frutas y Verduras"
 						newcarne = false;
 						newfrutaV = true;
 						newjuguete = false;
 						newpan = false;
-
 					} else {
+						// Muestra un mensaje de alerta si el índice ingresado no es válido
 						vf.getCon().mostrarAlerta("Ingrese un indice valido, recuerde "
 								+ "que no puede ser negativo ni exceder el tamaño de la lista");
-
 					}
 				}
-				if (juguete == true) {
+
+				// Verifica si la sección seleccionada es "Juguetes"
+				if (juguete) {
+					// Cambia el título de la ventana para indicar que se está actualizando un
+					// producto en "Juguetes"
 					vf.getPrincipal().setTitle("ACTUALIZANDO JUGUETES");
+					// Solicita al usuario que ingrese la posición del producto que desea actualizar
 					index = Integer.parseInt(vf.getCon().leerEntradaEmergente("Ingrese la posición a actualizar: "));
+					// Verifica si el índice ingresado es válido
 					if (mf.getJugueteDAO().checkearIndex(index - 1).equals("s")) {
+						// Oculta el botón de "Agregar" y muestra el botón de "Actualizar" para editar
+						// el producto
 						vf.getPrincipal().getPanelAgregar().getBtnAgregar().setVisible(false);
 						vf.getPrincipal().getPanelAgregar().getBtnActualizar().setVisible(true);
+						// Muestra el panel de actualización con los datos del producto seleccionado
 						vf.getPrincipal().mostrarPanelAgregar();
+						// Ajusta las variables booleanas para indicar que se está actualizando un
+						// producto de "Juguetes"
 						newcarne = false;
 						newfrutaV = false;
 						newjuguete = true;
 						newpan = false;
 					} else {
+						// Muestra un mensaje de alerta si el índice ingresado no es válido
 						vf.getCon().mostrarAlerta("Ingrese un indice valido, recuerde "
 								+ "que no puede ser negativo ni exceder el tamaño de la lista");
-
 					}
 				}
+
 				if (pan == true) {
 					vf.getPrincipal().getPanelAgregar().getAtributoPropio1().setText("Si / No");
 					vf.getPrincipal().getPanelAgregar().getAtributoPropio2().setText("Si / No");
