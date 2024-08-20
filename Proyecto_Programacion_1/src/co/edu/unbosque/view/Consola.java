a0579724b69e0733bdbba46d99b0d46a087148d3
Familia <Familia@RodrAvilHP>
2024-08-19 21:32:19 -0500

    correcion scanner

--- Proyecto_Programacion_1/src/co/edu/unbosque/view/Consola.java
+++ Proyecto_Programacion_1/src/co/edu/unbosque/view/Consola.java
@@ -1,12 +1,8 @@
 package co.edu.unbosque.view;
 
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 public class Consola {
-
-	private Scanner sc;
 
 	public void mostrarMensajeEmergente(String texto) {
 		JOptionPane.showMessageDialog(null, texto, "Mensaje", JOptionPane.INFORMATION_MESSAGE);
@@ -37,52 +33,6 @@ public class Consola {
 			return true;
 		}
 		return false;
-	}
-
-	public Consola() {
-		sc = new Scanner(System.in);
-	}
-
-	public void burnLine() {
-		sc.nextLine();
-	}
-
-	public String readLine() {
-		return sc.nextLine();
-	}
-
-	public String readWord() {
-		return sc.next();
-	}
-
-	public int readInt() {
-		return sc.nextInt();
-	}
-
-	public long readLong() {
-		return sc.nextLong();
-	}
-
-	public float readFloat() {
-		return sc.nextFloat();
-	}
-
-	public boolean readBoolean() {
-		String input = sc.nextLine();
-		if (input.toLowerCase().contains("si")) {
-			return true;
-		}
-		return false;
-	}
-
-	////////////////////////////////////////////////////
-
-	public void printNewLine(String text) {
-		System.out.println(text);
-	}
-
-	public void printSameLine(String text) {
-		System.out.print(text);
 	}
 
 }
